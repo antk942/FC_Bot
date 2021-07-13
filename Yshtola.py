3ae750c318dfcd4e7cd521a1840aefa19ceeda37
antk942 <antk942@gmail.com>
2021-07-13 16:53:23 +0300

    New private message on join

--- Yshtola.py
+++ Yshtola.py
@@ -53,8 +53,7 @@ animEm = Settings.AnimatedEmojDic
 
 IDsDic = Settings.IDsDic
 
-
-"""# region Heroku magic
+# region Heroku magic
 print("ctypes - Find opus:")
 a = ctypes.util.find_library('opus')
 print(a)
@@ -66,7 +65,7 @@ print(b)
 print("Discord - Is loaded:")
 c = discord.opus.is_loaded()
 print(c)
-# endregion"""
+# endregion
 
 
 @bot.event
@@ -85,12 +84,31 @@ async def on_message(message):
 
 @bot.event
 async def on_member_join(member):
-    if member.guild.id == 824112000342032385:
+    if member.guild.id == 779311642919436299:
         try:
             role = discord.utils.get(member.guild.roles, name="The New Guys")
             await member.add_roles(role)
         except:
             await bot.get_channel(824436047593209858).send("Could not give role for some reason, investigate")
+        try:
+            emb = discord.Embed(title="Welcome to The Other Guys.",
+                                description="Hello, before you start your adventure you should check out some things.",
+                                color=Settings.generalColorEMB)
+            emb.add_field(name="Rules.", value="Make sure you read carefully our rules.\n"
+                                               "[Rules](https://discord.com/channels/824112000342032385/824123037028188160/824488287976161320)", inline=False)
+            emb.add_field(name="Assign roles.", value="Check out all the cool roles you can have.\n"
+                                                      "[Assign roles](https://discord.com/channels/824112000342032385/841952676317233182/841955569153998888)", inline=False)
+            emb.add_field(name="Events.", value="There you will find about all the nice events we organize.\n"
+                                                "Some of them being mount farms glamour competition etc.", inline=False)
+            emb.add_field(name="Mount farms.", value="Extreme trials mounts:\n"
+                                                     "[Extremes](https://discord.com/channels/824112000342032385/830135464526610532/863706056637808671)\n"
+                                                     "Legacy mounts:\n"
+                                                     "[Legacy](https://discord.com/channels/824112000342032385/855136480120143912/863706006176399390)")
+            emb.set_footer(text="Make sure if you have any questions to ask.",
+                           icon_url=Settings.botIcon)
+            await member.send(embed=emb)
+        except:
+            await bot.get_channel(824436047593209858).send("Could not send message to the new guy, investigate")
 
 
 @bot.command()
@@ -150,53 +168,13 @@ async def DailyRefreshes():
 @bot.event
 async def on_ready():
     print('bot ready')
-    #await bot.get_channel(824436047593209858).send(IDsDic["Kon"])
+    # await bot.get_channel(824436047593209858).send(IDsDic["Kon"])
     await bot.wait_until_ready()
     ShowGMTPresence.start()
     DailyRefreshes.start()
     print('Servers connected to:')
     for guild in bot.guilds:
         print(guild.name)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 # token
